dfb75723636a62e2ba31ce8316c8ff4b1c22bf7b
Elchanan Shuky Shukrun <elnn.sh@gmail.com>
2020-01-01 10:12:09 +0200

    update TODO for phExtensionRequestDecision

--- src/server/DBConnection.java
+++ src/server/DBConnection.java
@@ -232,7 +232,7 @@ public class DBConnection {
             currPhase.setDeadLine(rs.getDate("phDeadLine").toLocalDate());
             currPhase.setPhaseStatus(Phase.PhaseStatus.valueOf(rs.getString("phStatus")));
             currPhase.setExtensionRequest(rs.getBoolean("phExtensionRequestDecision"));
-            // TODO: handle phExtensionRequest
+            // TODO: handle phExtensionRequestDecision
 //            Date date = rs.getDate("phExceptionTime");
 //            if(date != null) {
 //                LocalDate exceptionDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
