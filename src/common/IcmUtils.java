83b2cf3a35146b10c673aca58cfd917b2f5cd07f
tomro91 <57315297+tomro91@users.noreply.github.com>
2020-01-18 23:04:20 +0200

    DOCUMEN

--- src/common/IcmUtils.java
+++ src/common/IcmUtils.java
@@ -23,7 +23,13 @@ public class IcmUtils {
         Main_Window,
         Change_Request_Summary
         }
-
+    /**
+     * Display confirmation message with title,header text and content text
+     * @param title
+     * @param headerText
+     * @param contentText
+     * @return all the options for buttons to press in the pop up
+     */
     public static  Optional<ButtonType> displayConfirmationMsg(String title, String headerText, String contentText) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle(title);
@@ -31,7 +37,12 @@ public class IcmUtils {
         alert.setContentText(contentText);
         return alert.showAndWait();
     }
-
+    /**
+     * Displays information message with title,header text and content text
+     * @param title
+     * @param headerText
+     * @param contentText
+     */
     public static void displayInformationMsg(String title, String headerText, String contentText) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle(title);
@@ -39,7 +50,12 @@ public class IcmUtils {
         alert.setContentText(contentText);
         alert.showAndWait();
     }
-
+    /**
+     * Displays error message with title,header text and content text
+     * @param title
+     * @param headerText
+     * @param contentText
+     */
     public static void displayErrorMsg(String title, String headerText, String contentText) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle(title);
@@ -47,7 +63,12 @@ public class IcmUtils {
         alert.setContentText(contentText);
         alert.showAndWait();
     }
-
+    /**
+     * Loads specific scenes
+     * @param clientUI
+     * @param sceneName
+     * @throws IOException
+     */
     public static void loadScene(ClientUI clientUI, IcmUtils.Scenes sceneName) throws IOException {
         String sceneTitle;
         String fxmlPath;
@@ -81,7 +102,15 @@ public class IcmUtils {
 
         loadScene(clientUI, sceneTitle, fxmlPath, width, height);
     }
-
+    /**
+     * Loads every scene
+     * @param clientUI
+     * @param sceneTitle
+     * @param fxmlPath
+     * @param width
+     * @param height
+     * @throws IOException
+     */
     public static void loadScene(ClientUI clientUI, String sceneTitle, String fxmlPath, int width, int height) throws IOException {
         System.out.println("Loading scene: " + sceneTitle);
         FXMLLoader loader = new FXMLLoader();
@@ -93,7 +122,15 @@ public class IcmUtils {
         primaryStage.show();
     }
 
-    
+    /**
+     * Loads pop up
+     * @param clientUI
+     * @param sceneTitle
+     * @param fxmlPath
+     * @param width
+     * @param height
+     * @throws IOException
+     */
     public static void popUpScene(ClientUI clientUI, String sceneTitle, String fxmlPath, int width, int height) throws IOException {
     	 System.out.println("Loading pop-up scene: " + sceneTitle);
          FXMLLoader loader = new FXMLLoader();
@@ -108,11 +145,17 @@ public class IcmUtils {
          popUp=popUpStage;
          popUpStage.showAndWait();
     }
-    
+    /**
+     * Sets pop up
+     * @param NewPopUp
+     */
     public static void setPopUp (Stage NewPopUp) {
     	IcmUtils.popUp=NewPopUp;
     }
-    
+    /**
+     * Gets pop up
+     * @return stage of pop up
+     */
     public static Stage getPopUp () {
     	return IcmUtils.popUp;
     }
