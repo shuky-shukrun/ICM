49f33efeb5470e4862410ce1c319ee417207c784
morskurka <57572844+morskurka@users.noreply.github.com>
2020-01-18 10:04:38 +0200

    add edit screen

---
+++ src/client/crDetails/supervisor/Edit/Edit.java
@@ -0,0 +1,47 @@
+package client.crDetails.supervisor.Edit;
+
+import client.ClientUI;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import server.ServerService;
+
+public class Edit implements ClientUI{
+
+	@FXML
+	private Button submitButton;
+	@FXML
+	private Button CloseButton;
+	@FXML
+	private TextField currDeadlineTextField;
+	@FXML
+	private DatePicker newDeadlineDatePicker;
+	@FXML
+	private TextArea reasonTextArea;
+	
+	
+	public void initialize() {
+		
+	}
+
+	@FXML
+	void submitEdit(ActionEvent event) {
+		
+	}
+
+	@FXML
+	void closeEdit(ActionEvent event) {
+		
+	}
+	
+	
+	@Override
+	public void handleMessageFromClientController(ServerService serverService) {
+		
+		
+	}
+
+}
